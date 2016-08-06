9afa6d65e79e8099152d98ee406554515dd34fca
jdafferner <james.dafferner@gmail.com>
2016-08-06 15:36:30 -0500

    Update Utilities.java

--- Utilities.java
+++ Utilities.java
@@ -1,4 +1,4 @@
-package prog3;
+package prog3;  //remove before loki submission.  Used by eclipse
 
 import java.io.BufferedReader;
 import java.io.File;
@@ -15,24 +15,25 @@ public class Utilities {
 	private static final int NF = 30;
 	private static final int NP = 300;
 	private static final int NR = 5000;
-
+	String line = null;
 	
 public boolean getLine() throws FileNotFoundException{
-
+// reads a line from prog3.input and sends it to setLine.
+// Method getLine will retrieve it for use by main() or other called methods orginating from main() such as Rnorm etc.
 	File file = new File("prog3.input");
 	FileInputStream fis = new FileInputStream(file);
 	
 	
 		try {
-			BufferedReader br = new BufferedReader(new InputStreamReader(fis));
-				String x = br.readLine();
-				if(x == "0"){
-					return false;
-				}
-				else{
-					setLine(x);
-					return true;
-				}
+		BufferedReader br = new BufferedReader(new InputStreamReader(fis));
+			String x = br.readLine();
+			if(x == "0"){
+				return false;
+			}
+			else{
+				setLine(x);
+				return true;
+			}
 			
 		} 
 		catch (IOException e) {
@@ -44,6 +45,10 @@ public boolean getLine() throws FileNotFoundException{
 }
  private void setLine(String s){
 	 String line = s; 
+ }
+ 
+ public String getLine(){
+ 	return line;
  }
  
 }
